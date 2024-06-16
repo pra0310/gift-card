3ea5bc19129b9b6cae2ae0eb8be3cf5213b05b74
pra0310 <prasharma0310@gmail.com>
2024-06-16 17:17:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function changeImg0() {
+    let displayImg = document.getElementById('img0');
+    if (displayImg.src.match('images/g.jpg')) {
+        displayImg.src = 'images/2.jpg';
+    } else {
+        displayImg.src = 'images/g.jpg';
+    }
+}
+
+function changeImg1() {
+    let displayImg = document.getElementById('img1');
+    if (displayImg.src.match('images/g.jpg')) {
+        displayImg.src = 'images/3.jpg';
+    } else {
+        displayImg.src = 'images/g.jpg';
+    }
+}
+
+function changeImg2() {
+    let displayImg = document.getElementById('img2');
+    if (displayImg.src.match('images/g.jpg')) {
+        displayImg.src = 'images/4.jpg';
+    } else {
+        displayImg.src = 'images/g.jpg';
+    }
+}
+
+function changeImg3() {
+    let displayImg = document.getElementById('img3');
+    if (displayImg.src.match('images/g.jpg')) {
+        displayImg.src = 'images/5.jpg';
+    } else {
+        displayImg.src = 'images/g.jpg';
+    }
+}
